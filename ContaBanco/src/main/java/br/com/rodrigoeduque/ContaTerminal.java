d3f2c05a22df06511f6749a3ac54b62e9ce081d5
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2024-11-24 12:34:51 -0300

    Lógica criada

--- ContaBanco/src/main/java/br/com/rodrigoeduque/ContaTerminal.java
+++ ContaBanco/src/main/java/br/com/rodrigoeduque/ContaTerminal.java
@@ -1,13 +1,30 @@
 package br.com.rodrigoeduque;
 
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("i = " + i);
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o numero da Conta: ");
+        Integer conta = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite o Nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o Saldo da Conta: ");
+        Double saldo =  Double.parseDouble(scanner.nextLine());
+
+
+        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " +
+                conta + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
+
         }
     }
-}
